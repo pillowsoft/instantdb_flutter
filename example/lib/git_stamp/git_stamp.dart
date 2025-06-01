9d27873a86a7b555af5e27393083e4af35072c2c
Brad Pillow <webmaster@pillowsoft.com>
2025-05-31 21:37:09 -0400

    gitignore

--- example/lib/git_stamp/git_stamp.dart
+++ example/lib/git_stamp/git_stamp.dart
@@ -1,6 +1,6 @@
 // GENERATED FILE. DO NOT EDIT.
 class GitStamp {
   static const String branch = 'main';
-  static const String sha = '15ad1f94ed28dc8e92f6c0176913409f083187ea';
-  static const String buildDate = '2025-06-01T00:29:01.062915Z';
+  static const String sha = 'fa25c9f0c3861e2951c676b92d783d7e22b6e68a';
+  static const String buildDate = '2025-06-01T01:31:02.929937Z';
 }
