484d72aa1d8e91342a46faed243432f62741cc22
Brad Pillow <webmaster@pillowsoft.com>
2025-08-27 15:08:07 -0400

    Remove git_stamp generated file and add to .gitignore

--- example/lib/git_stamp/git_stamp.dart
+++
@@ -1,6 +0,0 @@
-// GENERATED FILE. DO NOT EDIT.
-class GitStamp {
-  static const String branch = 'main';
-  static const String sha = '5622d274c3a3ea0550b8f6fa434d66ed4a54a33a';
-  static const String buildDate = '2025-06-01T13:47:42.624399Z';
-}
